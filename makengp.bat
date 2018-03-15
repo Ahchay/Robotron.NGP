0bbaeba0f6b02e0af56753cdffb6498dc140573c
chrisahchay@yahoo.com <chrisahchay@yahoo.com>
2018-03-15 10:27:05 +0000

    Syncronised makengp.bat

--- makengp.bat
+++ makengp.bat
@@ -1,5 +1,12 @@
-@path=%PATH%;C:\Data\Development\NGPC\t900\bin
-@SET THOME=C:\Data\Development\NGPC\t900\
-@PROMPT $P$_NGPC$G
-@make
-@cmd
+@echo off
+
+for /f %%p in ('cd') do set _currentpath=%%p
+cd ..
+for /f %%p in ('cd') do set _ngpcpath=%%p
+cd %_currentpath%
+
+path=%PATH%;%_ngpcpath%\t900\bin
+SET THOME=%_ngpcpath%\t900\
+PROMPT $P$_NGPC$G
+make
+cmd
